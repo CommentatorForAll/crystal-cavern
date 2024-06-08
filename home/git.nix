2378d20661c819c4dadcd202f336b691fb744c78
Commentator2.0 <commentator2.0@protonmail.com>
2024-06-08 09:47:44 +0000

    fix: missing semi

--- home/git.nix
+++ home/git.nix
@@ -24,7 +24,7 @@ _: {
     };
 
     aliases = {
-    	plog = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+      plog = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
     };
   };
 }
